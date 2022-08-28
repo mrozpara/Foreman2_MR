e939498118cf022ea486473a5a5e8efad1cac0da
mrozpara <miroslaw.rozpara@wp.pl>
2022-08-28 16:03:55 +0200

    Preset class => moved to separate file

---
+++ Foreman/DataCache/Preset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreman
+{
+	public class Preset : IEquatable<Preset>
+	{
+		public string Name { get; set; }
+		public bool IsCurrentlySelected { get; set; }
+		public bool IsDefaultPreset { get; set; }
+
+		public Preset(string name, bool isCurrentlySelected, bool isDefaultPreset)
+		{
+			Name = name;
+			IsCurrentlySelected = isCurrentlySelected;
+			IsDefaultPreset = isDefaultPreset;
+		}
+
+		public bool Equals(Preset other)
+		{
+			return this == other;
+		}
+	}
+}
--- Foreman/Forms/MainForm.cs
+++ Foreman/Forms/MainForm.cs
@@ -564,22 +564,4 @@ namespace Foreman
 		}
     }
 
-    public class Preset : IEquatable<Preset>
-	{
-		public string Name { get; set; }
-		public bool IsCurrentlySelected { get; set; }
-		public bool IsDefaultPreset { get; set; }
-
-		public Preset(string name, bool isCurrentlySelected, bool isDefaultPreset)
-		{
-			Name = name;
-			IsCurrentlySelected = isCurrentlySelected;
-			IsDefaultPreset = isDefaultPreset;
-		}
-
-		public bool Equals(Preset other)
-		{
-			return this == other;
-		}
-	}
 }
